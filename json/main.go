2080662a12b2c1f6686221237a1bd3a9bdec5a60
Pratham Handique <pa2289@srmist.edu.in>
2024-11-07 20:03:34 +0530

    json

---
+++ json/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct{
+	Name string `json:"name"`
+	Age int `json:"age"`
+	IsAdult bool `json:"is_adult"`
+}
+func main(){
+	person :=Person{Name:"Prince",Age:21,IsAdult:true}
+	fmt.Println(person)
+	//convert person into json uncoding(marshal)
+	jsonData,err :=json.Marshal(person)
+	if err!=nil{
+		fmt.Println("error while encoding json",err)
+		return
+	}
+	fmt.Println("the person data in json is",string(jsonData))
+	//decoding json data(unmarshal)
+	var personData Person 
+	err =json.Unmarshal(jsonData,&personData)
+	if err!=nil{
+		fmt.Println("error while decoding json",err)
+		return
+	}
+	fmt.Println("the person data is",personData)
+
+
+}
