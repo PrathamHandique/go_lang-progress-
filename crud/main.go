97b5989246be0a2a36eac1c4335af17f2f7fe10b
Pratham Handique <pa2289@srmist.edu.in>
2024-11-08 07:13:33 +0530

    post

--- crud/main.go
+++ crud/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	//"encoding/json"
 )
 
@@ -14,8 +16,7 @@ type Todo struct{
 	Completed bool `json:"completed"`
 }
 
-func main(){
-	fmt.Println("Hello World")
+func performGetRequest(){
 	res, err :=http.Get("https://jsonplaceholder.typicode.com/todos/1")
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -34,13 +35,50 @@ func main(){
 		return
 	}
 	fmt.Println(string(data))
-	
-	
-	// var todo Todo
+		// var todo Todo
 	// err =json.NewDecoder(res.Body).Decode(&todo)
 	// if err != nil {
 	// 	fmt.Println("Error:", err)
 	// 	return
 	// }
 	// 	fmt.Println(todo)
+}
+
+func performPostRequest(){
+	todo:=Todo{
+		UserId:23,
+		Id:1,
+		Title:"Hello World",
+		Completed:false,
+	}
+	//convert todo into json
+	jsonData,err :=json.Marshal(todo) 
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	//convert json data to string
+	jsonString :=string(jsonData)
+	//convert json  data to reader
+	jsonReader :=strings.NewReader(jsonString)
+	myUrl := "https://jsonplaceholder.typicode.com/todos"
+	//send post request
+	res, err :=http.Post(myUrl,"application/json",jsonReader)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer res.Body.Close()
+	data, _:= ioutil.ReadAll(res.Body)
+	fmt.Println(string(data))
+
+
+}
+
+func main(){
+	fmt.Println("Hello World")
+	//performGetRequest()
+	performPostRequest()
+
+	
 }
